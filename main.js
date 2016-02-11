93ada91634e9631de9ac207102cb6bbad6ecd3a5
andres-root <andres.root.coder@gmail.com>
2016-02-11 13:57:41 -0500

    update

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 (function() {
-	var frameNumber = 1;
-	var totalFrames = 100;	
-	var frameExt = '.png';
-	var frame = document.querySelector('.frame');
-	var image = frameNumber + frameExt;
-	frame.style.backgroundImage = 'url(img/' + image + ')';
+  'user strict';
+  var frameNumber = 1;
+  var totalFrames = 100;
+  var frameExt = '.jpg';
+  var frame = document.querySelector('.hrt-animated-video .frame');
+  var postframe = document.querySelector('.hrt-animated-video .frame.post');
+  var image = frameNumber + frameExt;
+  postframe.style.backgroundImage = 'url(../../img/frames/' + (frameNumber + 2) + frameExt + ')';
+  console.log('url(../../img/frames/' + (frameNumber + 2) + frameExt + ')');
+  frame.style.backgroundImage = 'url(../../img/frames/' + image + ')';
 
-	function animate() {
-		if (frameNumber == 13) {
-			frameNumber = 0;
-		}
-		frameNumber += 1
-		image = (frameNumber) + frameExt;
-		frame.style.backgroundImage = 'url(img/' + image + ')';
-	}
-	
-	frame.addEventListener('animationiteration', animate, false);
+  function animate() {
+    if (frameNumber == 48) {
+      frameNumber = 0;
+    }
+    frameNumber += 1
+    image = (frameNumber) + frameExt;
+    postframe.style.backgroundImage = 'url(../../img/frames/' + (frameNumber + 1) + frameExt + ')';
+    frame.style.backgroundImage = 'url(../../img/frames/' + image + ')';
+
+  }
+
+  frame.addEventListener('animationiteration', animate, false);
 })();
